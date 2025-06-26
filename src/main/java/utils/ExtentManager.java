6c78eb391ed999f381eb37402e051082c1bfebb6
Lakshmi Narayana Sindiri <41689013+LakshminarayanaNani@users.noreply.github.com>
2025-06-26 14:01:36 +0530

    Update ExtentManager.java
    Commiting Changes Of Extent Manager

--- src/main/java/utils/ExtentManager.java
+++ src/main/java/utils/ExtentManager.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import com.aventstack.extentreports.reporter.configuration.ViewName;
 
 public class ExtentManager {
 	
@@ -13,19 +15,28 @@ public class ExtentManager {
 	    public static ExtentReports getInstance() {
 	        if (extent == null) {
 	            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-	            String reportDir = System.getProperty("user.dir") + File.separator + "test-output" + File.separator + "reports";
+	            String reportDir = System.getProperty("user.dir")+File.separator+"target"+File.separator+"reports";
 	            new File(reportDir).mkdirs();
 
-	            String reportPath = reportDir + File.separator + "ExtentReport_" + timestamp + ".html";
+	            String reportPath = reportDir+File.separator+"ExtentReport_"+timestamp+".html";
 
 	            ExtentSparkReporter spark = new ExtentSparkReporter(reportPath);
 	            spark.config().setReportName("SauceDemo Test Execution");
 	            spark.config().setDocumentTitle("Automation Report");
-
+	            spark.config().setTheme(Theme.STANDARD);
+	          
+	            spark.viewConfigurer().viewOrder().as(new ViewName[]{
+	            	    ViewName.DASHBOARD, 
+	            	    ViewName.TEST, 
+	            	    ViewName.AUTHOR, 
+	            	    ViewName.CATEGORY, 
+	            	    ViewName.DEVICE
+	            	});
+	            
 	            extent = new ExtentReports();
 	            extent.attachReporter(spark);
 	            extent.setSystemInfo("Project", "Sauce Demo");
-	            extent.setSystemInfo("Senior QA Tester", "Lakshmi Narayana Sindiri");
+	            extent.setSystemInfo("Senior QA Automation Engineer", "Lakshmi Narayana Sindiri");
 	        }
 	        return extent;
 	    }
