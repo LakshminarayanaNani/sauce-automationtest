6363656e5d96fa9aa5e160e78470ecfc7494013c
Lakshmi Narayana Sindiri <41689013+LakshminarayanaNani@users.noreply.github.com>
2025-06-26 14:04:58 +0530

    Update TestListner.java
    Commiting Changes Of Listner Class

--- src/test/java/tests/TestListner.java
+++ src/test/java/tests/TestListner.java
@@ -3,46 +3,28 @@ package tests;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-
 import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.Status;
-
 import utils.ExtentManager;
 
 public class TestListner implements ITestListener {
-	private static final ThreadLocal<ExtentTest> extentTest = new ThreadLocal<>();
-	private ExtentReports extent;
+    private ExtentReports extent;
 
-	@Override
-	public void onStart(ITestContext context) {
-		extent = ExtentManager.getInstance();
-	}
+    @Override
+    public void onStart(ITestContext context) {
+        extent = ExtentManager.getInstance();
+    }
 
-	@Override
-	public void onTestStart(ITestResult result) {
-		ExtentTest test = extent.createTest(result.getMethod().getMethodName());
-		extentTest.set(test);
-	}
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
+    }
 
-	@Override
-	public void onTestSuccess(ITestResult result) {
-		extentTest.get().log(Status.PASS, "Test passed");
-	}
+    @Override public void onTestStart(ITestResult result) {}
+    @Override public void onTestSuccess(ITestResult result) {}
+    @Override public void onTestFailure(ITestResult result) {}
+    @Override public void onTestSkipped(ITestResult result) {}
 
-	@Override
-	public void onTestFailure(ITestResult result) {
-		extentTest.get().log(Status.FAIL, result.getThrowable());
-	}
-
-	@Override
-	public void onTestSkipped(ITestResult result) {
-		extentTest.get().log(Status.SKIP, "Test skipped");
-	}
-
-	@Override
-	public void onFinish(ITestContext context) {
-		extent.flush();
-	} 
-
+    public static ExtentReports getExtent() {
+        return ExtentManager.getInstance();
+    }
 }
