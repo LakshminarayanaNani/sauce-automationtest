ab82e69846621ade417f5b3a8fceb800c9f168ca
Lakshmi Narayana Sindiri <41689013+LakshminarayanaNani@users.noreply.github.com>
2025-06-30 19:30:29 +0530

    Update TestListner.java

--- src/test/java/tests/TestListner.java
+++ src/test/java/tests/TestListner.java
@@ -3,19 +3,27 @@ package tests;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+
 import com.aventstack.extentreports.ExtentReports;
+
 import utils.ExtentManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class TestListner implements ITestListener {
+
+    private static final Logger logger = LogManager.getLogger(TestListner.class);
     private ExtentReports extent;
 
     @Override
     public void onStart(ITestContext context) {
+        logger.info("Test Suite started.", context.getName());
         extent = ExtentManager.getInstance();
     }
 
     @Override
     public void onFinish(ITestContext context) {
+        logger.info("Test Suite finished. Flushing ExtentReports.", context.getName());
         extent.flush();
     }
 
