1e17d65dc00e2df035e0cb5a060c28fc63fdf04f
Lakshmi Narayana Sindiri <41689013+LakshminarayanaNani@users.noreply.github.com>
2025-06-30 19:29:13 +0530

    Update DriverManager.java

--- src/main/java/driver/DriverManager.java
+++ src/main/java/driver/DriverManager.java
@@ -13,21 +13,27 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import config.BrowserType;
 import config.ConfigReader;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class DriverManager {
     private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+    private static final Logger logger = LogManager.getLogger(DriverManager.class);
 
     public static WebDriver getDriver() {
         if (driver.get() == null) {
             String browserName = ConfigReader.get("browser").toUpperCase();
+            logger.info("Initializing browser: {}", browserName);
             BrowserType browser = BrowserType.valueOf(browserName);
 
             switch (browser) {
                 case CHROME -> {
+                    logger.debug("Setting up ChromeDriver...");
                     WebDriverManager.chromedriver().setup();
                     ChromeOptions options = new ChromeOptions();
 
                     boolean isHeadless = Boolean.parseBoolean(ConfigReader.get("chrome.headless"));
+                    logger.debug("Chrome headless mode: {}", isHeadless);
 
                     options.addArguments("--disable-notifications");
                     options.addArguments("--disable-extensions");
@@ -41,17 +47,18 @@ public class DriverManager {
 
                     WebDriver chromeDriver = new ChromeDriver(options);
                     driver.set(chromeDriver);
-
                     if (!isHeadless) {
                         chromeDriver.manage().window().maximize();
                     }
                 }
 
                 case FIREFOX -> {
+                    logger.debug("Setting up FirefoxDriver...");
                     WebDriverManager.firefoxdriver().setup();
                     FirefoxOptions options = new FirefoxOptions();
 
                     boolean isHeadless = Boolean.parseBoolean(ConfigReader.get("firefox.headless"));
+                    logger.debug("Firefox headless mode: {}", isHeadless);
 
                     if (isHeadless) {
                         options.addArguments("-headless");
@@ -61,17 +68,18 @@ public class DriverManager {
 
                     WebDriver firefoxDriver = new FirefoxDriver(options);
                     driver.set(firefoxDriver);
-
                     if (!isHeadless) {
                         firefoxDriver.manage().window().maximize();
                     }
                 }
 
                 case EDGE -> {
+                    logger.debug("Setting up EdgeDriver...");
                     WebDriverManager.edgedriver().setup();
                     EdgeOptions options = new EdgeOptions();
 
                     boolean isHeadless = Boolean.parseBoolean(ConfigReader.get("edge.headless"));
+                    logger.debug("Edge headless mode: {}", isHeadless);
 
                     if (isHeadless) {
                         options.addArguments("--headless=new");
@@ -84,10 +92,14 @@ public class DriverManager {
                     driver.set(edgeDriver);
                 }
 
-                default -> throw new RuntimeException("Unsupported browser: " + browserName);
+                default -> {
+                    logger.error("Unsupported browser: {}", browserName);
+                    throw new RuntimeException("Unsupported browser: " + browserName);
+                }
             }
 
             driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            logger.info("{} browser initialized successfully", browserName);
         }
 
         return driver.get();
@@ -95,8 +107,10 @@ public class DriverManager {
 
     public static void quitDriver() {
         if (driver.get() != null) {
+            logger.info("Quitting browser session...");
             driver.get().quit();
             driver.remove();
+            logger.info("Browser session terminated.");
         }
     }
 }
