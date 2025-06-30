9769c24fbe3819cf028580297d96c21d24eece1e
Lakshmi Narayana Sindiri <41689013+LakshminarayanaNani@users.noreply.github.com>
2025-06-30 19:29:53 +0530

    Update BaseTest.java

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,21 +1,31 @@
 package tests;
 
+import driver.DriverManager;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import config.ConfigReader;
-import driver.DriverManager;
+import org.testng.annotations.Parameters;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class BaseTest {
+
+    private static final Logger logger = LogManager.getLogger(BaseTest.class);
+
     protected WebDriver driver;
 
-    @BeforeMethod
-    public void setup() {
+    @Parameters({"browser"})
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() {
+        logger.info("Test setup starting...");
         driver = DriverManager.getDriver();
+        logger.info("Driver initialized.");
     }
 
-    @AfterMethod
-    public void teardown() {
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        logger.info("Test teardown starting...");
         DriverManager.quitDriver();
+        logger.info("Driver quit successfully.");
     }
 }
